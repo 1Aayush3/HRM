418cad99cbd989287466eea20ed807110a7ee85e
dhanmagar <dm01891715@student.ku.edu.np>
2019-12-09 16:47:45 +0545

    migrations changes

--- database/migrations/2019_12_09_080729_create_user_leave_apply_table.php
+++ database/migrations/2019_12_09_080729_create_user_leave_apply_table.php
@@ -13,6 +13,7 @@ class CreateUserLeaveApplyTable extends Migration
      */
     public function up()
     {
+        //tablename user_leave_applies kkk
         Schema::create('user_leave_applies', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('employee_id')->unsigned();
