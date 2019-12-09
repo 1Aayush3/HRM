c4954371ee916f298b7d7214a1776fa27494cd12
dhanmagar <dm01891715@student.ku.edu.np>
2019-12-09 14:00:07 +0545

    leave_migrations

---
+++ database/migrations/2019_12_09_080729_create_user_leave_apply_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserLeaveApplyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_leave_applies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('employee_id')->unsigned();
+            $table->foreign('employee_id')->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->bigInteger('leave_type_lists_id')->unsigned();
+            $table->foreign('leave_type_lists_id')->references('id')->on('leave_type__lists')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->text('description');
+            $table->enum('status', ['pending', 'accept', 'reject']);
+            $table->string('reject_reason');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_leave_applies');
+    }
+}
